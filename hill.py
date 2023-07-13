f6b886fc40ff170be1b7403e60b649d25b3b0fb1
mmaftuhashiddiqi <mmaftuh.2021@student.uny.ac.id>
2023-07-13 10:00:39 +0700

    hill encryption fix, semoga

--- hill.py
+++ hill.py
@@ -1,33 +1,27 @@
 # Main Program
 
-n = 4
+# hill encryption
+def encrypt(message, key):
 
-keyMatrix = [[0] * n for i in range(n)]
+	n = len(message)
 
-# Generate vector for the message
-messageVector = [[0] for i in range(n)]
+	# key matrix template
+	keyMatrix = [[0] * n for i in range(n)]
+	# message vector template
+	messageVector = [[0] for i in range(n)]
+	# cipher matrikx template
+	cipherMatrix = [[0] for i in range(n)]
 
-# Generate vector for the cipher
-cipherMatrix = [[0] for i in range(n)]
-
-# generate key matrix
-def getKeyMatrix(key):
-
+	# key matrix
 	k = 0
 	for i in range(n):
 		for j in range(n):
 			keyMatrix[i][j] = ord(key[k].upper()) % 65
 			k += 1
 
-# hill encryption
-def encrypt(message, key):
-
-	# matrix key
-	getKeyMatrix(key)
-
-	# Generate vector for the message
+	# generate message vector
 	for i in range(n):
-		messageVector[i][0] = ord(message[i]) % 65
+		messageVector[i][0] = ord(message[i].upper()) % 65
 
 	# cipher matrix
 	for i in range(n):
@@ -36,32 +30,47 @@ def encrypt(message, key):
 			for x in range(n):
 				cipherMatrix[i][j] += (keyMatrix[i][x] * messageVector[x][j])
 			cipherMatrix[i][j] = cipherMatrix[i][j] % 26
-	# print(cipherMatrix)
 
-	# cipher string
+	# cipher text
 	CipherText = []
 	for i in range(n):
 		CipherText.append(chr(cipherMatrix[i][0] + 65))
 
-	# cipher array to cipher text
+	# cipher array to cipher string
 	return "".join(CipherText)
+
+# hill decryption
+def decrypt(cipherMessage, key):
+	print("bentar, belum nemu")
 
 
 # CLI Program
 import os
 
 while True:
-	os.system('clear') # for macos
-	# os.system('cls') # for winos
+    os.system('clear') # for macos
+    # os.system('cls') # for winos
 
-	if True:
-		message = input('\nMasukkan kalimat: ')
-		key = input('Masukkan kunci: ')
-		print('the encryption is:', encrypt(message, key))
+    print('Pilih: \n1. Enkripsi Hill\n2. Dekripsi Hill')
+    try:
+        choices = int(input('\nMasukkan nomor pilihan: '))
+    except:
+        choices = 0
 
-	isEnough = input('\nudah belooonnn (y/t)?')
-	if isEnough == 'y' or isEnough == 'Y':
-		break
+    if choices == 1:
+        message = input('\nMasukkan kalimat: ')
+        key = input('Masukkan kunci: ')
+        print("Hasil enkripsi:", encrypt(message, key))
+    elif choices == 2:
+        cipherMessage = input('\nMasukkan kalimat sandi: ')
+        key = input('Masukkan key: ')
+        print("Hasil dekripsi:", decrypt(cipherMessage, key))
+    else:
+        print('\nmasukkin pilihannya yang bener saattt!!!')
+
+    isEnough = input('\nudah belooonnn (y/t)?')
+    if isEnough == 'y' or isEnough == 'Y':
+        break
 
 # message = "ACTA"
 # key = "GYBNQKURPABCDEFG"
